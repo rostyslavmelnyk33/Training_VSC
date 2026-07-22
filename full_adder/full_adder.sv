d992a6efdcfef41bb7f097be452ee2c1ea48cd5b
Rostyslav Melnyk <rostyslavmelnyk33@gmail.com>
2026-07-22 16:26:38 +0300

    Added module using always_comb full_adder.sv

--- full_adder/full_adder.sv
+++ full_adder/full_adder.sv
@@ -8,3 +8,17 @@ module full_adder (input logic a, b, cin,
   assign cout = q | (p & cin);
 
 endmodule
+
+/*
+module fulladder(input logic a, b, cin,
+output logic s, cout);
+logic p, g;
+always_comb
+begin
+p = a ^ b; // блокирующее
+g = a & b; // блокирующее
+s = p ^ cin; // блокирующее
+cout = g |(p & cin); // блокирующее
+end
+endmodule
+*/
