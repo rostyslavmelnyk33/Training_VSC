0a32779badf853cb2647007ef8e0e2e23ea2aaea
Rostyslav Melnyk <rostyslavmelnyk33@gmail.com>
2026-07-22 15:30:48 +0300

    Improve latch testbench

--- latch/tb_latch.sv
+++ latch/tb_latch.sv
@@ -11,10 +11,22 @@ module tb;
         $monitor("At time %t, clk = %b, d = %b, q = %b", $time, clk, d, q);
         clk = 0;
         d = 4'b0000;
-        #5 d = 4'b1010;
-        #5 d = 4'b1100;
-        #5 d = 4'b1111;
-        #5 $finish;
+        #2 d = 4'b0001;
+        #2 d = 4'b0010;
+        #2 d = 4'b0011;
+        #2 d = 4'b0100;
+        #2 d = 4'b0101;
+        #2 d = 4'b0110;
+        #2 d = 4'b0111;
+        #2 d = 4'b1000;
+        #2 d = 4'b1001;
+        #2 d = 4'b1010;
+        #2 d = 4'b1011;
+        #2 d = 4'b1100;
+        #2 d = 4'b1101;
+        #2 d = 4'b1110;
+        #2 d = 4'b1111;
+        #2 $finish;
     end
-    
-    always #1 clk = ~clk;
+    always #5 clk = ~clk;
+endmodule
